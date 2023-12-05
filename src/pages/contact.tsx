3da48ed25fb9e103981cd671494c9d69969a05c9
Mizuki Sango <msageha+github@gmail.com>
2023-12-05 18:09:27 +0900

    remove public

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,12 +3,63 @@ import Layout from "../components/Layout";
 import SEOHead from "../components/head";
 import { getSize } from "../utils/size";
 import { getColor } from "../utils/color";
+import { ChangeEvent, useState } from "react";
+import axios from "axios";
 
 const titleClasses = `mb-4 ${getSize("large")} ${getColor("secondary")}`;
 const subTitleClasses = `mb-4 ${getSize("medium")} ${getColor("primary")}`;
 const descriptionClasses = `mb-4 ${getSize("small")} ${getColor("secondary")}`;
 
 const ContactForm = () => {
+    const [popupMessage, setPopupMessage] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    };
+    const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    };
+    const handleSubjectChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSubject(e.target.value);
+    };
+    const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.target.value);
+    };
+    const handleSubmit = () => {
+        setLoading(true);
+        const submitParams = new FormData();
+
+        submitParams.append(FormKeys.name, name);
+        submitParams.append(FormKeys.email, email);
+        submitParams.append(FormKeys.subject, subject);
+        submitParams.append(FormKeys.message, message);
+
+        axios
+            .post(FormKeys.formUrl, submitParams)
+            .then(() => {
+                setLoading(false);
+                setPopupMessage("お問い合わせが送信されました！");
+            })
+            .catch(() => {
+                setLoading(false);
+                setPopupMessage("お問い合わせが送信されました！");
+                console.log("エラーです");
+            });
+    };
+    const FormKeys = {
+        formUrl:
+            "https://docs.google.com/forms/d/e/1FAIpQLScozi1Pvyvxvy9p2k1R2xfmHrO2NovWLNgJ6277NH8AxP2aGQ/formResponse",
+        name: "entry.1696594161",
+        email: "entry.92481834",
+        subject: "entry.1279078928",
+        message: "entry.1064825457",
+    };
+
     return (
         <div className="my-10 justify-center sm:py-12">
             <div className="relative py-3 sm:max-w-3xl sm:mx-auto">
@@ -25,8 +76,10 @@ const ContactForm = () => {
                         <input
                             className="shadow mb-4 appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                             type="text"
+                            id="name"
                             placeholder="お名前"
                             name="name"
+                            onChange={handleNameChange}
                         />
 
                         <input
@@ -34,6 +87,7 @@ const ContactForm = () => {
                             type="email"
                             placeholder="Email"
                             name="email"
+                            onChange={handleEmailChange}
                         />
 
                         <input
@@ -41,6 +95,7 @@ const ContactForm = () => {
                             type="text"
                             placeholder="件名"
                             name="_subject"
+                            onChange={handleSubjectChange}
                         />
 
                         <textarea
@@ -48,6 +103,7 @@ const ContactForm = () => {
                             placeholder="メッセージを入力してください"
                             name="message"
                             style={{ height: "121px" }}
+                            onChange={handleMessageChange}
                         ></textarea>
 
                         <div className="flex justify-start">
@@ -55,9 +111,27 @@ const ContactForm = () => {
                                 className="shadow bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                                 type="submit"
                                 value="Send ➤"
+                                onClick={handleSubmit}
+                                disabled={loading}
                             />
                         </div>
                     </form>
+
+                    {popupMessage && (
+                        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                            <div className="bg-white p-4 rounded shadow-md">
+                                <p className={subTitleClasses}>
+                                    {popupMessage}
+                                </p>
+                                <button
+                                    className="mt-2 bg-blue-500 text-white p-2 rounded"
+                                    onClick={() => setPopupMessage(null)}
+                                >
+                                    閉じる
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
